5e126b3edb387c5ab434df4afcaada706d6ffafe
magnusjacobsen <magnusjacobsen@gmx.com>
2021-01-25 16:48:56 +0100

    working on SVMTool

--- src/taggers/svmtool.py
+++ src/taggers/svmtool.py
@@ -93,3 +93,47 @@ class SVMT(SysCallTagger):
                 glob_exclude_files.add(filename)
         
         return list(glob_all_files - glob_exclude_files)
+
+    def evaluate(self, ext=""):
+        """
+        Before this method is called, the given tagger being evaluated will have outputted its
+        predictions to a file. This method then runs through that file, and outputs 
+        sentence and token accuracy of the tagger's predictions.
+        Individual taggers can override this method, if the predictions they output are
+        formatted differently, than what this method expects.
+        """
+        total = 0
+        correct = 0
+        curr_sent_correct = 0
+        curr_sent_count = 0
+        correct_sent = 0
+        total_sent = 0
+
+        test_data = data_archives.get_dataset_path(self.args.lang, self.args.treebank, "test", simplified=self.simplified_dataset, eos=self.simplified_eos_dataset)
+
+        print(test_data)
+        #exit()
+
+        with open(self.predict_path() + ext, "r", encoding="utf-8") as fp:
+            lines = fp.readlines()
+            for line in lines:
+                line = line.strip()
+                if line == "":
+                    total_sent += 1
+                    if curr_sent_count == curr_sent_correct:
+                        correct_sent += 1
+                    curr_sent_correct = 0
+                    curr_sent_count = 0
+                    continue
+                total += 1
+                curr_sent_count += 1
+                split = line.split(None)
+                predicted = split[-1]
+                actual = split[-2]
+                if predicted == actual:
+                    correct += 1
+                    curr_sent_correct += 1
+
+        token_acc = correct / total if total > 0 else 0
+        sent_acc = correct_sent / total_sent if total_sent > 0 else 0
+        return token_acc, sent_acc
